cfa3360b4989f09c45caf89d9215b64b91c3477d
TheDarkStrix <thedarkstrix@gmail.com>
2017-08-19 11:09:27 +0000

    CyanogenMod -> LineageOS

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2015 The CyanogenMod Project
+# Copyright 2016 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +29,7 @@ $(call inherit-product, device/xiaomi/kenzo/device.mk)
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := cm_kenzo
+PRODUCT_NAME := lineage_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
